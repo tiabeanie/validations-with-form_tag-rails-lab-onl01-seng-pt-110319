6b50c25a38cbd083dc2c12208da1c7ec584b83b8
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-04-15 18:02:12 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :authors, only: %i[show new edit create update]
+  resources :posts, only: %i[show new edit create update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
